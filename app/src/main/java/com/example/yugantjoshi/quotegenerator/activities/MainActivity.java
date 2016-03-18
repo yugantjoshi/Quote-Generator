4aecea0c9d9370e3522699b229c2d55293b386c3
Yugant Joshi <joshi.yugant@gmail.com>
2016-03-18 00:19:59 -0400

    function quote generator. Touch the quote to generate a new one.

--- app/src/main/java/com/example/yugantjoshi/quotegenerator/activities/MainActivity.java
+++ app/src/main/java/com/example/yugantjoshi/quotegenerator/activities/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.yugantjoshi.quotegenerator.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import com.example.yugantjoshi.quotegenerator.R;
 import com.example.yugantjoshi.quotegenerator.data.QuotesInterface;
 import com.example.yugantjoshi.quotegenerator.data.model.Quote;
@@ -19,37 +21,61 @@ public class MainActivity extends AppCompatActivity {
     private static final String BASE_URL = "http://api.forismatic.com/api/1.0/";
 
     QuotesInterface quoteService;
+    TextView quoteTextView, authorTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        initRetrofit();
+
+        quoteTextView = (TextView) findViewById(R.id.quote_text);
+        authorTextView = (TextView) findViewById(R.id.author_text);
+
+        createRetrofit();
+        generateQuote();
+
+        quoteTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateQuote();
+            }
+        });
     }
 
-    private void initRetrofit() {
+    private void createRetrofit()
+    {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        quoteService = retrofit.create(QuotesInterface.class);
+        this.quoteService = retrofit.create(QuotesInterface.class);
     }
 
-    @OnClick(R.id.main_activity__fetch_quote_button)
-    public void fetchQuotesButtonClicked() {
-        int anyKeyIDontKnowWhatTheKeyIs = 555;
-        quoteService.getQuote(anyKeyIDontKnowWhatTheKeyIs).enqueue(new Callback<Quote>() {
+    public void generateQuote()
+    {
+        int key=0;
+        this.quoteService.getQuote(key).enqueue(new Callback<Quote>() {
             @Override
-            public void onResponse(final Call<Quote> call, final Response<Quote> response) {
-                Toast.makeText(getApplicationContext(), response.body().toString(), Toast.LENGTH_LONG).show();
-            }
+            public void onResponse(Call<Quote> call, Response<Quote> response) {
+                String quoteText;
+                String authorText;
 
+                quoteText =  response.body().quoteText.toString();
+                authorText = response.body().quoteAuthor.toString();
+
+                quoteTextView.setGravity(Gravity.CENTER);
+                authorTextView.setGravity(Gravity.CENTER);
+
+                quoteTextView.setText(quoteText);
+                authorTextView.setText(authorText);
+            }
             @Override
-            public void onFailure(final Call<Quote> call, final Throwable t) {
-                Toast.makeText(getApplicationContext(), "Rest call to get quote failed!", Toast.LENGTH_LONG).show();
+            public void onFailure(Call<Quote> call, Throwable t) {
+                Toast.makeText(MainActivity.this,R.string.quote_fail,Toast.LENGTH_LONG).show();
+                t.printStackTrace();
             }
         });
     }
+
 
 }
